ae565d0c8c16073952379550918af8f121871d67
Stian Didriksen <stipsan@gmail.com>
2016-08-15 01:29:34 +0200

    made it configurable when to prepend theme file

--- index.js
+++ index.js
@@ -2,21 +2,20 @@ var loaderUtils = require('loader-utils')
 var path = require('path')
 var fs = require('fs')
 
-// @TODO update regex to include sass
-var uikitPattern = /node_modules\/uikit\/.+less$/
 
 module.exports = function (source) {
   if (this.cacheable) this.cacheable()
   var callback = this.async()
 
-  // Only act on uikit less imports
-  if (!uikitPattern.test(this.resourcePath)) {
+  var config = loaderUtils.getLoaderConfig(this, 'uikitLoader')
+
+  // Only act on uikit less imports by default
+  var testPattern = config.test || /node_modules\/uikit\/.+less$/
+  if (!testPattern.test(this.resourcePath)) {
     return callback(null, source)
   }
 
-  var config = loaderUtils.getLoaderConfig(this, 'uikitLoader')
   var themeName = config.theme
-
   if (!themeName) {
     return callback(new Error('No `theme` specified in the uikit-loader options! See: https://github.com/stipsan/uikit-loader#usage'))
   }
